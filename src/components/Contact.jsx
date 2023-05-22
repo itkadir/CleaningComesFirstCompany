bec581fa48e17f1bd495a9860ce8f75edee3aa6b
itkadir <yaaden695@gmail.com>
2023-05-22 20:23:34 +0100

    updated

---
+++ src/components/Contact.jsx
@@ -0,0 +1,17 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+const Contact = () => {
+  return (
+    <div id="content">
+      <h1>Contact Us</h1>
+      <p>
+        If you have any enquiries, please contact us on 08458802429 . Alternatively you can email us at enquiries@cleaningcomesfirst.co.uk  and one of our team will get back you shortly.
+      </p>
+
+
+    </div >
+  );
+};
+
+export default Contact;
